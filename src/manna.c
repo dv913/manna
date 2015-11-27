69d917e1ee5d86912612836a6bb53222f5b23a64
Me <dmitry.veselov13@imperial.ac.uk>
2015-11-27 02:31:59 +0000

    Backup of all Manna files before system reinstallation

--- src/manna.c
+++ src/manna.c
@@ -4,35 +4,37 @@
 
 #include<mt.h>
 
-#define L 5 // length of the lattice minus 1
-#define C 2 // top up cell number in array count
-#define N_ROLLS 10  // number of experiments -- balls to drop
+#define LENGTH (5) // length of the lattice minus
+#define CENTRE 2 // top up cell number
+#define NUMBER_OF_ROLLS 100  // number of experiments -- balls to drop
+#define double X[] = {1/2, 1/2}
+#define int Y[] = {-1, 1}
 
 int main()  {
  
-  int lattice[L] = {[0 ... L-1] = 0};
+  int lattice[LENGTH] = {[0 ... LENGTH-1] = 0};
   int i, drop;
   int k;
-  for(k = 0; k<L; k++) {printf("%i", lattice[k]); }
+  for(k = 0; k<LENGTH; k++) {printf("%i", lattice[k]); }
   printf(" --- End of initialisation\n");   
 
   sgenrand(time(NULL));
 
-  for (drop = 0; drop < N_ROLLS; drop++) {  // big droppping cycle
+  for (drop = 0; drop < NUMBER_OF_ROLLS; drop++) {  // big droppping cycle
 
-  lattice[C] = lattice[C] + 1;  //actual drop in the drop location
+  lattice[CENTRE] = lattice[CENTRE] + 1;  //actual drop in the drop location
 
   printf("\n");
-  for(k = 0; k<L; k++) {printf("%i", lattice[k]); } // check after initial drop
+  for(k = 0; k<LENGTH; k++) {printf("%i", lattice[k]); } // check after initial drop
   printf( " --- After %i drops************************************", drop+1);  
 
-  for (i = 0; i < L; i++) {
+  for (i = 0; i < LENGTH; i++) {
 
     if (lattice[i] > 1) //check slots > 1
      {  
       // draw 2 rand nums
-      int dist1 = genrand()*2; 
-      int dist2 = genrand()*2;
+      int dist1 = genrand()*3; 
+      int dist2 = genrand()*3;
       
        // convert to distr
 
@@ -46,13 +48,13 @@ int main()  {
       printf("\nBall 2 from cell %i moves: %i  (%i,%i)\n", i, (int)dist2*(int)dir2, (int)dist2,(int)dir2);
 
       // shoot 2 balls
-      if(i+dist1*dir1 < L && i+dist1*dir1 >= 0) {lattice[i+(int)dist1*(int)dir1] = lattice[i+(int)dist1*(int)dir1] + 1;}
-      if(i+dist2*dir2 < L && i+dist2*dir1 >= 0) {lattice[i+(int)dist2*(int)dir2] = lattice[i+(int)dist2*(int)dir2] + 1;}
+      if(i+dist1*dir1 < LENGTH && i+dist1*dir1 >= 0) {lattice[i+(int)dist1*(int)dir1] = lattice[i+(int)dist1*(int)dir1] + 1;}
+      if(i+dist2*dir2 < LENGTH && i+dist2*dir1 >= 0) {lattice[i+(int)dist2*(int)dir2] = lattice[i+(int)dist2*(int)dir2] + 1;}
       // decrease initial slot by 2
       lattice[i] = lattice[i] - 2;
       
       printf("Final standing after shooting:  ");
-      for(k = 0; k<L; k++) {printf("%i", lattice[k]); } 
+      for(k = 0; k<LENGTH; k++) {printf("%i", lattice[k]); } 
 
       // restart array check from beginning
       i = -1 ;  // account for autoincrement
